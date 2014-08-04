19e40ec32000fb9d9397cf3e1fd9a891159e65a1
Hannes Hapke <hannes.hapke@gmail.com>
2014-08-04 08:25:01 -0700

    Change to CreateView, as well as intro of leaflet for the mapping

--- fruitloop_django/apps/fruit/views.py
+++ fruitloop_django/apps/fruit/views.py
@@ -1,31 +1,18 @@
-from django.views.generic.list import ListView
-from django.views.generic.edit import FormMixin
+from django.views.generic.edit import CreateView
+from django.core.urlresolvers import reverse_lazy
 
 from .models import FruitLocation
 from .forms import FruitLocationForm
 
-class FruitLocationListView(FormMixin, ListView):
+class FruitLocationCreateView(CreateView):
 
     model = FruitLocation
-    form_class = FruitLocationForm
+    form = FruitLocationForm
+    success_url = reverse_lazy('home')
+    template = 'fruit/fruitlocation_list.html'
+    fields = ['address', 'fruit_type', 'comment']
 
-
-    def get(self, request, *args, **kwargs):
-        # From ProcessFormMixin
-        form_class = self.get_form_class()
-        self.form = self.get_form(form_class)
-
-        # From BaseListView
-        self.object_list = self.get_queryset()
-        allow_empty = self.get_allow_empty()
-        if not allow_empty and len(self.object_list) == 0:
-            raise Http404(_(u"Empty list and '%(class_name)s.allow_empty' is False.")
-                      % {'class_name': self.__class__.__name__})
-
-        context = self.get_context_data(object_list=self.object_list, form=self.form)
-        return self.render_to_response(context)
-
-    # def get_context_data(self, **kwargs):
-    #     context = super(ArticleListView, self).get_context_data(**kwargs)
-    #     context['now'] = timezone.now()
-    #     return context
+    def get_context_data(self, **kwargs):
+        context = super(FruitLocationCreateView, self).get_context_data(**kwargs)
+        context['object_list'] = FruitLocation.objects.all()
+        return context   
--- fruitloop_django/urls.py
+++ fruitloop_django/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-from fruit.views import FruitLocationListView
+from fruit.views import FruitLocationCreateView
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', FruitLocationListView.as_view(), name='home'),
+    url(r'^$', FruitLocationCreateView.as_view(), name='home'),
     url(r'^admin/', include(admin.site.urls)),
 )
